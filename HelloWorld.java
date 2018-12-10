7804feb36099047a6355521122ccbb26040ada56
Mon <C18843@ISD-PF0ZGYJW.gdcusaadc.com>
2018-12-10 20:58:09 +0800

    Modified HelloWorld.class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
     public static void main(String args[]) {
         for(int i = 1; i <= 10; i++){
             System.out.println("Hello World");
-            System.out.println("What is the meaning of life");
+            System.out.println("Is this the meaning of life");
         }
     }    
 }
