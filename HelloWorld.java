4abb3936742080a652b9f4c7652bf941d98d5453
Mon <C18843@ISD-PF0ZGYJW.gdcusaadc.com>
2018-12-10 21:01:54 +0800

    Modified HelloWorld class

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld {
     public static void main(String args[]) {
         for(int i = 1; i <= 10; i++){
             System.out.println("Hello World");
-            System.out.println("Is this the meaning of life");
         }
+        
+        System.out.println("Mon Philippe Avila");
     }    
 }
